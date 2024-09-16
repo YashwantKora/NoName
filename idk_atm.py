29057b97e294511e64569b3fb661ab2d5b6d2356
YashwantKora <146644924+YashwantKora@users.noreply.github.com>
2024-09-16 20:02:06 +0530

    Add files via upload

--- idk_atm.py
+++ idk_atm.py
@@ -3,6 +3,70 @@ from tkinter import ttk
 from tkinter import messagebox #<--not in use
 from datetime import date
 
+#Age Calculator
+
+def Age_Calculator():
+    age_window = tk.Toplevel()
+    age_window.title('Days Since')
+    age_window.geometry('1080x720')
+    mainframe4 = tk.Frame(age_window, bg='black')
+    mainframe4.pack(fill='both', expand=True)
+
+    def Calculate():
+        current_date = date.today()
+        year = int(year_box.get())
+        month = int(month_box.get())
+        day = int(day_box.get())
+        DOB = date(year, month, day)
+        age = current_date - DOB
+        yage = age/365
+        age_label.config(text=f"You Are {age} Old!" if yage == 1 else f"You Are {age} old!")
+        age2_label.config(text=f"OR {yage} Years Old")
+        
+
+
+    label = ttk.Label(mainframe4, text="Enter your DOB")
+    label.grid(row=0,column=0)
+
+    year_box = ttk.Entry(mainframe4)
+    year_box.grid(row=1, column=0)
+
+    y_l = ttk.Label(mainframe4, text="YYYY")
+    y_l.grid(row=2,column=0)
+
+    month_box = ttk.Entry(mainframe4)
+    month_box.grid(row=1, column=1)
+    
+    m_l = ttk.Label(mainframe4, text="MM")
+    m_l.grid(row=2,column=1)
+
+    day_box = ttk.Entry(mainframe4)
+    day_box.grid(row=1, column=2)
+
+    d_l = ttk.Label(mainframe4, text="DD")
+    d_l.grid(row=2,column=2)
+
+    cal_button = ttk.Button(mainframe4, text="Calculate", command=Calculate)
+    cal_button.grid(row=3, column=1)
+
+    age_label = ttk.Label(mainframe4, text="...")
+    age_label.grid(row=4, column=1)
+
+    age2_label = ttk.Label(mainframe4, text="")
+    age2_label.grid(row=5, column=1)
+
+
+
+
+#To Do List
+
+def To_do_list():
+    TODO = tk.Toplevel()
+    TODO.title('Days Since')
+    TODO.geometry('1080x720')
+
+#Time Since 
+
 def Time_since_window():
     time_since_window = tk.Toplevel()
     time_since_window.title('Days Since')
@@ -12,11 +76,11 @@ def Time_since_window():
         current_year = int(txt_box_3.get())
         current_month = int(txt_box_2.get())
         current_day = int(txt_box_1.get())
-        preset_date = date(2024,9,7)
+        preset_date = date(2024,9,15)
         current_date = date(current_year, current_month, current_day)
         difference = current_date - preset_date
-        days = abs(difference)
-        lbl_8.config(text=f"{days}")
+        day = abs(difference)
+        lbl_8.config(text=f"{day}") if current_date == current_date or current_date == (2024,9,16) else lbl_8.config(text=f"{day}s")
 
     mainframe3 = tk.Frame(time_since_window, bg='black')
     mainframe3.pack(fill='both', expand=True)
@@ -48,7 +112,7 @@ def Time_since_window():
     lbl_5 = ttk.Label(mainframe3, text="Preset Date: ", background='black', foreground='white', font=('poppins', 20))
     lbl_5.grid(row=3, column=0, pady=20,sticky="W")
 
-    lbl_6 = ttk.Label(mainframe3, text="09-07-2024", background='black', foreground='white', font=('poppins', 30))
+    lbl_6 = ttk.Label(mainframe3, text="15-09-2024", background='black', foreground='white', font=('poppins', 30))
     lbl_6.grid(row=4, column=1)
 
     lbl_7 = ttk.Label(mainframe3, text="It's Been: ", background='black', foreground='white', font=('poppins', 20))
@@ -57,11 +121,7 @@ def Time_since_window():
     lbl_8 = ttk.Label(mainframe3, text="...", background='black', foreground='white', font=('poppins', 30))
     lbl_8.grid(row=7, column=1)
 
-
-
-    
-
-
+#Temperature Converter
 
 def temperature_window():
     temperature_window = tk.Toplevel()
@@ -102,9 +162,10 @@ def temperature_window():
     result_label = ttk.Label(mainframe2, text="...", background='black', foreground='white', font=('poppins', 25) )
     result_label.grid(row=7, column=0, pady= 20, padx= 20) 
 
+#Main Menu
 
 window = tk.Tk()
-window.geometry('1000x600')
+window.geometry('800x400')
 window.title("I have no idea what i am doing")
 
 mainframe = tk.Frame(window, bg='black',)
@@ -120,16 +181,24 @@ button1.grid(row=1, column=0, pady=20, padx=40)
 bt1_label = ttk.Label(mainframe, text="- Converts Fahrenheit to Celsius and vice versa!", background='black', foreground='white', font=('poppins', 15))
 bt1_label.grid(row=1, column=1)
 #                                       lol v
-button2 = ttk.Button(mainframe, text="           To Do List           ")
+button2 = ttk.Button(mainframe, text="           To Do List           ", command=To_do_list)
 button2.grid(row=2, column=0, pady = 20)
 #                                                           bruh!! v 
-bt2_label = ttk.Label(mainframe, text="- To do list!                                                     ", background='black', foreground='white', font=('poppins', 15))
+bt2_label = ttk.Label(mainframe, text="- Create and organize your tasks", background='black', foreground='white', font=('poppins', 15))
 bt2_label.grid(row=2, column=1)
 
 button3 = ttk.Button(mainframe, text="         Time Since           ", command=Time_since_window)
 button3.grid(row=3, column=0, pady=20)
 
-bt3_label = ttk.Label(mainframe, text="- How many Days have passed!                        ", background='black', foreground='white', font=('poppins', 15))
+bt3_label = ttk.Label(mainframe, text="- How many Days have passed!", background='black', foreground='white', font=('poppins', 15))
 bt3_label.grid(row=3, column=1)
+
+button4 = ttk.Button(mainframe, text="       Age Calculator       ", command=Age_Calculator)
+button4.grid(row=4, column=0, pady=20)
+
+bt4_label = ttk.Label(mainframe, text="- Calculates your age!", background='black', foreground='white', font=('poppins', 15))
+bt4_label.grid(row=4, column=1)
+
+
 window.mainloop()
 
